f47c8df72999cb3bdfef168ef91f56b72b9b8111
Saadman <saadmansakib34@gmail.com>
2025-10-31 02:46:53 +0600

    New Push

---
+++ Variables.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Saadman Sakib";
+    $age = 24;
+
+    echo $name. '<br>';
+    echo $age;
+    ?>
+</body>
+</html>
